a4ddada932ab07b109b0540631a265bc721312ce
nonon <nononcrust@gmail.com>
2023-09-28 21:55:28 +0900

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button>버튼</Button>
+    </main>
+  );
+}
---
+++ src/config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "유니버스 피아노",
+  description: "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "공지사항",
+      href: "/notices",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/ui",
+    docs: "https://ui.shadcn.com",
+  },
+};
