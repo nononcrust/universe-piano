13c3238e0fc917c4c1c7897741cb5f94e6a4ab36
nonon <nononcrust@gmail.com>
2023-10-01 13:10:42 +0900

    카카오 로그인 기능 수정

--- src/app/auth/kakao/callback/page.tsx
+++ src/app/auth/kakao/callback/page.tsx
@@ -1,6 +1,7 @@
 import { kakaoApi } from "@/api/kakao";
 import { RedirectWithUser } from "@/components/redirect-with-user";
 import { ROUTE } from "@/lib/constants/route";
+import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
 
 export default async function KakaoCallbackPage({
@@ -17,15 +18,26 @@ export default async function KakaoCallbackPage({
   try {
     const data = await kakaoApi.getAccessToken(code);
     const userInfo = await kakaoApi.getUserInfo(data.access_token);
+    const kakaoId = String(userInfo.id);
 
-    const user = {
+    const user = await prisma.user.findUnique({
+      where: {
+        kakaoId,
+      },
+    });
+
+    if (!user) {
+      return redirect(`${ROUTE.SIGNUP}?token=${data.access_token}`);
+    }
+
+    const userState = {
       id: userInfo.id,
       nickname: userInfo.properties.nickname,
       email: "test@gamil.com",
       profileImage: userInfo.properties.profile_image,
     };
 
-    return <RedirectWithUser user={user} />;
+    return <RedirectWithUser user={userState} />;
   } catch (error) {
     return redirect(ROUTE.LOGIN);
   }
