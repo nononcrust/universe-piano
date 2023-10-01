81e9a61a97f41032557ea966d1fe37fbb3e27b34
nonon <nononcrust@gmail.com>
2023-10-01 22:44:36 +0900

    og image, apple-touch-icon 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,21 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "유니버스 피아노 - 미국 음대 입시의 모든것",
   description: "유니버스 피아노 홈페이지",
+  openGraph: {
+    title: "유니버스 피아노",
+    type: "website",
+    locale: "ko_KR",
+    url: "https://universe-piano.vercel.app",
+    siteName: "유니버스 피아노",
+    description: "미국 음대 입시의 모든것, 유니버스 피아노",
+    images: [
+      {
+        url: "/images/og-image.jpg",
+        width: 1200,
+        height: 628,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
