f3117a715b0c7df40531b2a1c69688c9e5cee963
nonon <nononcrust@gmail.com>
2023-10-18 21:54:39 +0900

    shadcn plugin 분리

---
+++ src/lib/shadcn-preset.ts
@@ -0,0 +1,9 @@
+import type { Config } from "tailwindcss";
+import animatePlugin from "tailwindcss-animate";
+import { shadcnPlugin } from "./shadcn-plugin";
+
+export const shadcnPreset = {
+  darkMode: ["class"],
+  content: [],
+  plugins: [animatePlugin, shadcnPlugin],
+} satisfies Config;
