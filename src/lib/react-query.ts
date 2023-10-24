23776a265414ba096813d76d4dfe874643058b26
nonon <nononcrust@gmail.com>
2023-10-24 12:53:53 +0900

    react query 설정 변경

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,4 +1,16 @@
 import { QueryClient } from "@tanstack/react-query";
 import { cache } from "react";
 
-export const getQueryClient = cache(() => new QueryClient());
+export const getQueryClient = cache(
+  () =>
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          retry: false,
+          refetchOnReconnect: false,
+          staleTime: 1000 * 60 * 5,
+        },
+      },
+    }),
+);
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,23 +1,12 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { getQueryClient } from "@/lib/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren, useState } from "react";
 import { RecoilRoot } from "recoil";
 
 export const Providers = ({ children }: PropsWithChildren) => {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            refetchOnWindowFocus: false,
-            retry: false,
-            refetchOnReconnect: false,
-            staleTime: 1000 * 60 * 5,
-          },
-        },
-      }),
-  );
+  const [queryClient] = useState(getQueryClient);
 
   return (
     <RecoilRoot>
