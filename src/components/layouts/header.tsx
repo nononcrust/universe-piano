4896c0b6ff361308041877fad95430999b2583c2
nonon <nononcrust@gmail.com>
2023-10-28 17:06:38 +0900

    ui 수정

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -25,52 +25,54 @@ export const Header = () => {
 
   return (
     <>
-      <header className="sticky top-0 z-40 w-full border-b bg-background">
-        <div className="container flex h-16 items-center justify-between space-x-4 sm:space-x-0">
-          <NavigationMenuDialog />
-          <div className="flex gap-12">
-            <Link
-              href={ROUTE.HOME}
-              className="absolute left-1/2 top-0 flex h-16 -translate-x-1/2 items-center md:static md:left-auto md:translate-x-0"
-            >
-              <Image src="/images/logo.svg" width={40} height={40} alt="사이트 로고" />
-              <span className="inline-block font-bold">{siteConfig.name}</span>
-            </Link>
-            <nav className="hidden gap-6 md:flex">
-              {siteConfig.mainNav.map((item, index) => (
-                <Link
-                  key={index}
-                  href={item.href}
-                  className={cn(
-                    "flex items-center text-sm font-medium text-muted-foreground transition hover:text-foreground",
-                    pathname.startsWith(item.href) && "text-foreground",
-                  )}
-                >
-                  {item.title}
-                </Link>
-              ))}
-            </nav>
-          </div>
-          {!user && (
-            <Link
-              href={ROUTE.LOGIN}
-              className={cn("text-sm", fetchStatus === "fetching" && "invisible")}
-            >
-              로그인
-            </Link>
-          )}
-          {user && (
-            <div className="flex items-center gap-3">
-              {/* <Link href={ROUTE.PAYMENT} className="hidden sm:flex">
+      <header className="sticky top-0 z-40 w-full bg-background">
+        <div className="border-b">
+          <div className="container flex h-16 items-center justify-between space-x-4 sm:space-x-0">
+            <NavigationMenuDialog />
+            <div className="flex gap-12">
+              <Link
+                href={ROUTE.HOME}
+                className="absolute left-1/2 top-0 flex h-16 -translate-x-1/2 items-center md:static md:left-auto md:translate-x-0"
+              >
+                <Image src="/images/logo.svg" width={40} height={40} alt="사이트 로고" />
+                <span className="inline-block font-bold">{siteConfig.name}</span>
+              </Link>
+              <nav className="hidden gap-6 md:flex">
+                {siteConfig.mainNav.map((item, index) => (
+                  <Link
+                    key={index}
+                    href={item.href}
+                    className={cn(
+                      "flex items-center text-sm font-medium text-muted-foreground transition hover:text-foreground",
+                      pathname.startsWith(item.href) && "text-foreground",
+                    )}
+                  >
+                    {item.title}
+                  </Link>
+                ))}
+              </nav>
+            </div>
+            {!user && (
+              <Link
+                href={ROUTE.LOGIN}
+                className={cn("text-sm", fetchStatus === "fetching" && "invisible")}
+              >
+                로그인
+              </Link>
+            )}
+            {user && (
+              <div className="flex items-center gap-3">
+                {/* <Link href={ROUTE.PAYMENT} className="hidden sm:flex">
               <Icon.ShoppingCart className="h-8 w-8 rounded-full p-1.5 transition duration-200 hover:bg-gray-100" />
             </Link> */}
-              <UserMenu />
-            </div>
-          )}
+                <UserMenu />
+              </div>
+            )}
+          </div>
         </div>
+        {pathname.startsWith(ROUTE.REVIEW.LIST) && <CategoryTab categories={REVIEW_CATEGORIES} />}
+        {pathname.startsWith(ROUTE.MYPAGE.HOME) && <CategoryTab categories={MYPAGE_CATEGORIES} />}
       </header>
-      {pathname.startsWith(ROUTE.REVIEW.LIST) && <CategoryTab categories={REVIEW_CATEGORIES} />}
-      {pathname.startsWith(ROUTE.MYPAGE.HOME) && <CategoryTab categories={MYPAGE_CATEGORIES} />}
     </>
   );
 };
