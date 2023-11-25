e7b494752223840f0f2baee9ac1a556618abae23
nonon <nononcrust@gmail.com>
2023-11-25 22:35:20 +0900

    폰트 최적화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,62 +3,11 @@ import { UserInfoFetcher } from "@/components/user-info-fetcher";
 import { siteConfig } from "@/configs/site";
 import { Providers } from "@/providers";
 import type { Metadata, Viewport } from "next";
-import localFont from "next/font/local";
 import { Toaster } from "sonner";
 import "swiper/css";
 import "swiper/css/pagination";
+import "../styles/font.css";
 import "../styles/globals.css";
-
-const pretendard = localFont({
-  src: [
-    {
-      path: "../assets/fonts/Pretendard-ExtraLight.woff2",
-      weight: "100",
-      style: "normal",
-    },
-    {
-      path: "../assets/fonts/Pretendard-Thin.woff2",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "../assets/fonts/Pretendard-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../assets/fonts/Pretendard-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../assets/fonts/Pretendard-Medium.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../assets/fonts/Pretendard-SemiBold.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../assets/fonts/Pretendard-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../assets/fonts/Pretendard-ExtraBold.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "../assets/fonts/Pretendard-Black.woff2",
-      weight: "900",
-      style: "normal",
-    },
-  ],
-  display: "block",
-});
 
 export const revalidate = 0;
 
@@ -78,7 +27,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko" className={pretendard.className}>
+    <html lang="ko">
       <head />
       <body className="tracking-tight antialiased">
         <Providers>
