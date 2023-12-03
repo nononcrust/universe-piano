79e83d04a3a64ac29e90c474abf3b0b38ee7b21a
nonon <nononcrust@gmail.com>
2023-12-03 19:36:37 +0900

    ui 수정

--- src/components/notice/notice-list-item.tsx
+++ src/components/notice/notice-list-item.tsx
@@ -7,7 +7,7 @@ interface NoticeListItemProps {
 
 export const NoticeListItem = ({ title, createdAt }: NoticeListItemProps) => {
   return (
-    <li className="flex cursor-pointer flex-col py-4 transition md:hover:translate-x-2">
+    <li className="flex cursor-pointer flex-col py-4 transition">
       <p className="text-lg font-medium">{title}</p>
       <p className="text-sm text-muted-foreground">{createdAt}</p>
     </li>
