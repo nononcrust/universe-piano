20d919686c99155a1f68ba55af935e4c85609e6c
nonon <nononcrust@gmail.com>
2023-12-09 17:39:41 +0900

    미국 음대 입시 과외 페이지 이미지 추가

--- src/app/(main)/service/tutoring/page.tsx
+++ src/app/(main)/service/tutoring/page.tsx
@@ -1,25 +1,16 @@
-import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 
 export default function TutoringListPage() {
   return (
-    <main className="flex flex-col">
-      <section className="bg-gray-100">
-        <div className="container mt-16 flex flex-col items-center gap-8">
-          <div className="flex flex-1 flex-col items-center gap-4">
-            <Badge>미국 음대 입시 과외</Badge>
-            <h1 className="text-center text-2xl font-bold leading-tight md:text-4xl">
-              입시 경험자들이 직접
-              <br />
-              과외를 진행합니다
-            </h1>
-          </div>
-          {/* <div className="aspect-square w-full rounded-xl bg-gray-200 md:max-w-[260px]" /> */}
-        </div>
-        <div className="flex justify-center">
-          <Image src="/images/3d-star.png" width={300} height={300} alt="별" />
-        </div>
-      </section>
+    <main className="flex flex-col items-center">
+      <Image src="/images/tutoring/tutoring-1.jpg" width={860} height={1000} alt="" />
+      <Image src="/images/tutoring/tutoring-2.jpg" width={860} height={1100} alt="" />
+      <Image src="/images/tutoring/tutoring-3.jpg" width={860} height={1100} alt="" />
+      <Image src="/images/tutoring/tutoring-4.jpg" width={860} height={600} alt="" />
+      <Image src="/images/tutoring/tutoring-5.jpg" width={860} height={830} alt="" />
+      <Image src="/images/tutoring/tutoring-6.jpg" width={860} height={1100} alt="" />
+      <Image src="/images/tutoring/tutoring-7.jpg" width={860} height={1000} alt="" />
+      <Image src="/images/tutoring/tutoring-8.jpg" width={860} height={250} alt="" />
     </main>
   );
 }
