66e4a5a5692bd532d1afe694ecc57effa1629ebe
nonon <nononcrust@gmail.com>
2023-12-14 20:34:00 +0900

    sentry global error 설정

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,12 @@
 "use client";
 
-export default function GlobalErrorPage() {
+import * as Sentry from "@sentry/nextjs";
+import { useEffect } from "react";
+
+export default function GlobalErrorPage({ error }: { error: Error & { digest?: string } }) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
   return <main className="flex min-h-screen w-full items-center justify-center">에러 페이지</main>;
 }
