b59c0c20b8f64cb9ba68b6443cd72683040c9e37
nonon <nononcrust@gmail.com>
2023-12-15 15:27:03 +0900

    폰트 설정 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,38 @@ import { siteConfig } from "@/configs/site";
 import { GoogleAnalytics } from "@/lib/google-analytics";
 import { Providers } from "@/providers";
 import type { Metadata, Viewport } from "next";
+import localFont from "next/font/local";
 import { Toaster } from "sonner";
 import "swiper/css";
 import "swiper/css/pagination";
 import "../styles/globals.css";
 
 export const revalidate = 0;
+
+const pretendard = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Pretendard-Regular.subset.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard-Medium.subset.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard-SemiBold.subset.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard-Bold.subset.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
 
 export const viewport: Viewport = {
   width: "device-width",
@@ -26,7 +52,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.className}>
       <head>
         <GoogleAnalytics />
       </head>
