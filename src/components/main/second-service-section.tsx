f877bbf8f9dae4d0c8f7356b7e32f9489bc871ed
nonon <nononcrust@gmail.com>
2023-12-25 22:35:55 +0900

    ui 수정

--- src/components/main/second-service-section.tsx
+++ src/components/main/second-service-section.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import auditionResultImage from "@/assets/images/landing/audition-result.png";
-import kitImage from "@/assets/images/landing/kit.png";
-import scholarshipImage from "@/assets/images/landing/scholarship.png";
 import { ROUTE } from "@/constants/route";
-import Image from "next/image";
 import Link from "next/link";
+import { ColoredIcon } from "../colored-icon";
 import { Icon } from "../icon";
 import { Badge } from "../ui/badge";
 import { Card } from "./card";
@@ -31,17 +28,17 @@ export const SecondServiceSection = () => {
 const AuditionResultCard = () => {
   return (
     <Link href={ROUTE.NEWS.AUDITION.LIST}>
-      <Card className="group relative cursor-pointer overflow-hidden border transition hover:shadow-lg">
+      <Card className="max-h-[160px] min-h-[160px] cursor-pointer transition md:hover:shadow-lg">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
-            <Card.Title>오디션 결과 발표</Card.Title>
+            <Card.Title className="flex items-center">
+              <ColoredIcon.Notice className="h-6 w-6" />
+              <p className="ml-2">오디션 결과 발표</p>
+            </Card.Title>
           </div>
           <Icon.ArrowRight className="h-6 w-6 rounded-full bg-gray-200 p-1" />
         </div>
         <Card.Subtitle>로그인 후에 미국 음대 오디션 결과를 확인 하실 수 있습니다.</Card.Subtitle>
-        <div className="flex justify-center">
-          <Image className="mt-6" width={200} src={auditionResultImage} alt="audition" />
-        </div>
       </Card>
     </Link>
   );
@@ -49,11 +46,14 @@ const AuditionResultCard = () => {
 
 const ScholarshipCard = () => {
   return (
-    <Card className="overflow-hidden">
+    <Card className="max-h-[160px] min-h-[160px]">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-2">
           {/* <Icon.BookOpen className="h-5 w-5 fill-primary text-primary" /> */}
-          <Card.Title>장학금 증액 컨설팅</Card.Title>
+          <Card.Title className="flex items-center">
+            <ColoredIcon.Trophy className="h-6 w-6" />
+            <p className="ml-2">장학금 증액 컨설팅</p>
+          </Card.Title>
           <Badge className="bg-white" variant="outline">
             준비중
           </Badge>
@@ -63,27 +63,24 @@ const ScholarshipCard = () => {
       <Card.Subtitle className="text-gray-400">
         광고, 조작 없음. 결과로 증명합니다. 최초 장학금 $30000까지 증액 성공!
       </Card.Subtitle>
-      <div className="flex justify-center">
-        <Image className="mt-6" width={200} src={scholarshipImage} alt="scholarship" />
-      </div>
     </Card>
   );
 };
 
 const AuditionKitCard = () => {
   return (
-    <Card className="overflow-hidden">
+    <Card className="max-h-[160px] min-h-[160px]">
       <div className="flex items-center gap-2">
         {/* <Icon.Leaf className="h-5 w-5 fill-primary text-primary" /> */}
-        <Card.Title>오디션 119 키트</Card.Title>
+        <Card.Title className="flex items-center">
+          <ColoredIcon.Storage className="h-6 w-6" />
+          <p className="ml-2">오디션 119 키트</p>
+        </Card.Title>
         <Badge className="bg-white" variant="outline">
           준비중
         </Badge>
       </div>
       <Card.Subtitle>미국 음대 오디션 투어 꿀팁, 상황별 영어 표현, 체크리스트까지!</Card.Subtitle>
-      <div className="flex justify-center">
-        <Image className="mt-6" width={200} src={kitImage} alt="kit" />
-      </div>
     </Card>
   );
 };
