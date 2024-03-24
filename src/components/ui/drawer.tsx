f05dc74e91265bb83cdb02f89f46b4a13abfd393
nonon <nononcrust@gmail.com>
2024-03-24 20:34:03 +0900

    버그 수정

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -39,7 +39,6 @@ const DrawerVariants = cva(
     "data-[state=open]:animate-in data-[state=closed]:animate-out",
     "data-[state=closed]:duration-300 data-[state=open]:duration-500",
   ),
-
   {
     variants: {
       side: {
@@ -48,7 +47,7 @@ const DrawerVariants = cva(
           "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
         left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
         right:
-          "inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
+          "inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
       },
     },
     defaultVariants: {
@@ -111,7 +110,11 @@ const DrawerDescription = React.forwardRef<
   React.ElementRef<typeof DrawerPrimitives.Description>,
   React.ComponentPropsWithoutRef<typeof DrawerPrimitives.Description>
 >(({ className, ...props }, ref) => (
-  <DrawerPrimitives.Description ref={ref} className={cn("text-sm text-sub", className)} {...props} />
+  <DrawerPrimitives.Description
+    ref={ref}
+    className={cn("text-sm text-sub", className)}
+    {...props}
+  />
 ));
 DrawerDescription.displayName = DrawerPrimitives.Description.displayName;
 
