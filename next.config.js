8373b4d8e1d93e12f045e73f81a6d27e507c60c1
nonon <dev.nonon@gmail.com>
2024-05-01 11:36:17 +0900

    next config 변경

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
